355d10c5b1b372997686f4170eade52ad904cd26
Luis Cabrera <gigocabrera@outlook.com>
2024-03-09 10:42:29 -0500

    improvements

--- DataAccess.cs
+++ DataAccess.cs
@@ -4,6 +4,7 @@
 // gigocabrera@outlook.com
 //
 
+using System;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
@@ -14,6 +15,11 @@ namespace CoreCumulativeReorderReport
     class DataAccess
     {
         string conn = ConfigurationManager.ConnectionStrings["arxConnection"].ToString();
+
+        static DateTime startMonth = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+        static DateTime endMonth = startMonth.AddMonths(1).AddTicks(-1);
+        static DateTime start90days = startMonth.AddMonths(-3);
+        static DateTime end90days = start90days.AddMonths(3).AddTicks(-1);
 
         public DataTable GetDataTable(string sql)
         {
@@ -110,15 +116,32 @@ namespace CoreCumulativeReorderReport
         public int GetCurrentRenewal(int repid)
         {
             StringBuilder sb = new StringBuilder();
-            sb.Append("SELECT ");
-            sb.Append("    COUNT(DISTINCT(WO.PATIENTID)) AS TOTAL ");
-            sb.Append("FROM AR1ORDW WO ");
-            sb.Append("INNER JOIN AR1PAT PAT ON PAT.ID = WO.PATIENTID ");
-            sb.Append("WHERE WO.LASTDATEBILLED BETWEEN DATEADD(mm, DATEDIFF(mm, 0, GETDATE()), 0) AND EOMONTH(GETDATE()) ");
-            sb.Append("AND PAT.PATIENTCATEGORY = 'CGM' ");
-            sb.Append("AND WO.BILLTYPE = 'P' ");
-            sb.Append("AND PAT.PATIENTSTATUS = 'A' ");
-            sb.Append("AND PAT.SALESID = " + repid + "; ");
+            sb.Append("SELECT SUM(TOTAL) AS TOTAL ");
+            sb.Append("FROM( ");
+            //             Handle 30-day orders
+            sb.Append("    SELECT ");
+            sb.Append("        COUNT(DISTINCT(WO.PATIENTID)) AS TOTAL ");
+            sb.Append("    FROM AR1ORDW WO ");
+            sb.Append("    WHERE WO.LASTDATEBILLED BETWEEN DATEADD(mm, DATEDIFF(mm, 0, GETDATE()), 0)  AND DATEADD(second, -1, DATEADD(month, DATEDIFF(month, 0, GETDATE()) + 1, 0)) ");
+            sb.Append("        AND WO.BILLTYPE = 'P' ");
+            sb.Append("        AND WO.ITEMID IN(1957, 1950, 1951, 1962) ");
+            sb.Append("        AND WO.RECORDTYPE = 'M' ");
+            sb.Append("        AND WO.PATIENTID IN( ");
+            sb.Append("            SELECT ID FROM AR1PAT PAT WHERE PAT.PATIENTCATEGORY = 'CGM' AND PAT.PATIENTSTATUS = 'A' AND PAT.SALESID = ").Append(repid);
+            sb.Append("        ) ");
+            sb.Append("    UNION ALL ");
+            //             Handle 90-day orders
+            sb.Append("    SELECT ");
+            sb.Append("        COUNT(DISTINCT(WO.PATIENTID)) AS TOTAL ");
+            sb.Append("    FROM AR1ORDW WO ");
+            sb.Append("    WHERE WO.LASTDATEBILLED BETWEEN DATEADD(MONTH, DATEDIFF(MONTH, 0, DATEADD(MONTH, -3, GETDATE())), 0)  AND DATEADD(second, -1, DATEADD(month, DATEDIFF(month, 0, GETDATE()) + 1, 0)) ");
+            sb.Append("        AND WO.BILLTYPE = 'P' ");
+            sb.Append("        AND WO.ITEMID IN(1957, 1950, 1951, 1962) ");
+            sb.Append("        AND WO.RECORDTYPE = 'Q' ");
+            sb.Append("        AND WO.PATIENTID IN( ");
+            sb.Append("            SELECT ID FROM AR1PAT PAT WHERE PAT.PATIENTCATEGORY = 'CGM' AND PAT.PATIENTSTATUS = 'A' AND PAT.SALESID = ").Append(repid);
+            sb.Append("        ) ");
+            sb.Append(") AS CombinedResults; ");
             return GetCount(sb.ToString());
         }
 
